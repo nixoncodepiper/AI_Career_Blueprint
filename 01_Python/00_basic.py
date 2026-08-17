5fcc7201b92c156f0a8db6f7fffdcbd391ff0561
nixoncodepiper <nixes.de313antony@gmail.com>
2026-08-17 20:56:47 +0200

    Practice Python print statements

--- 01_Python/00_basic.py
+++ 01_Python/00_basic.py
@@ -3,3 +3,7 @@ print("My name is Nixon.")
 print("Welcome to my AI Career Blueprint!")
 print("I am learning Python.")
 print("I will become an AI Engineer.")
+name="nixon"
+age=22
+print(name)
+print(age)
